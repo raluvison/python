030cf8a28003bf94121ab6217d7c81498f4d1b90
Rafael Luvison <luvison.25@gmail.com>
2025-10-09 13:26:22 -0300

    desafio 4

--- aula 6/desafio4.py
+++ aula 6/desafio4.py
@@ -1,5 +1,17 @@
 n1 = input('Digite algo: ')
 
 print (
-    'Seu tipo primitivo é: ' , (type(n1))
+    f'Seu tipo primitivo é: {type(n1)} \n'
+    f'Ele é alphanumérico?? {n1.isalnum()} \n'
+    f'Ele é um alpha? {n1.isalpha()} \n'
+    f'Ele é ? {n1.isascii()} \n'
+    f'Ele é decimal? {n1.isdecimal()} \n'
+    f'Ele é um digito? {n1.isdigit()} \n'
+    f'Ele é {n1.isidentifier()} \n'
+    f'Ele está todo em minúsculo? {n1.islower()} \n'
+    f'Ele é numérico? {n1.isnumeric()} \n'
+    f'Ele {n1.isprintable()} \n'
+    f'Ele {n1.isspace()} \n'
+    f'Ele está todo em maiúsculo? {n1.isupper()} \n'
+    f'Ele é um título? {n1.istitle()}'
 )
