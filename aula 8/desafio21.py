6303b4dc8df471c251b696be4a7ea1e95dd1b465
Rafael Luvison <luvison.25@gmail.com>
2025-10-15 13:04:12 -0300

    desafio21

--- aula 8/desafio21.py
+++ aula 8/desafio21.py
@@ -1,3 +1,7 @@
-import playsound3
+import pygame
 
-playsound("C:\Users\luvison\Documents\estudos\python\aula 8\musica\avicii.mp3")
+pygame.mixer.init()
+pygame.mixer.music.load(r'C:\Users\luvison\Documents\estudos\python\aula 8\desafio21.mp3')
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
