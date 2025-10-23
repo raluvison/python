f7961c62189a57d0194a0efda9f2eaa1bdc0907d
Rafael Luvison <luvison.25@gmail.com>
2025-10-22 23:58:59 -0300

    desafio 32

--- aula 10/desafio32-v2.py
+++ aula 10/desafio32-v2.py
@@ -1,8 +1,9 @@
 ano = int(input('Digite um ano e eu direi se ele é bissexto: '))
-if ano % 4 == 0:
+if ano % 400 == 0:
     print('Ele é bissexto!')
-elif ano[2:] == 00:
-    ano % 400 == 0
+elif ano % 100 == 0:
+    print('Ele não é bissexto!')
+elif ano % 4 == 0:
     print('Ele é bissexto!')
 else:
     print('Ele não é bissexto!')
