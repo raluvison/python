5a53a3b9dc236438c45279261349e92e138b8c20
Rafael Luvison <luvison.25@gmail.com>
2025-10-26 00:21:14 -0300

    desafio 29 corrigido

--- aula 10/desafio29.py
+++ aula 10/desafio29.py
@@ -2,3 +2,4 @@ vel = int(input('Digite a velocidade que você passou no radar: '))
 multa = (vel-80)*7
 if vel >= 80:
     print(f'Você foi multado! A multa ficou no valor de: R${multa:.2f}')
+print(f'Velocidade medida: {vel}Km/h\nTenha um ótimo dia!')
