7513ff83f9e19b52422452f500607d19fe470253
Rafael Luvison <luvison.25@gmail.com>
2025-10-29 23:09:46 -0300

    desafio 38

---
+++ aula 12/desafio38.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+if n1 > n2:
+    print('O PRIMEIRO valor é MAIOR!')
+elif n2 > n1:
+    print('O SEGUNDO valor é MAIOR!')
+else:
+    print('Os dois valor são IGUAIS!')
