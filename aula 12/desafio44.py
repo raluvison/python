2fa0fcb0f5f1a72c2f41341c2415fb1085d5b457
Rafael Luvison <luvison.25@gmail.com>
2025-11-03 12:59:58 -0300

    desafio 44 corrigido

--- aula 12/desafio44.py
+++ aula 12/desafio44.py
@@ -17,20 +17,21 @@ while True:
 
     if opção == 1:
         total = valor - desconto10
-        print(f'Sua compra de R${valor:.2f} vai custar R${total:.2f} no final.')
         break
     elif opção == 2:
-        print(f'Sua compra de R${valor:.2f} vai custar R${valor - desconto5:.2f} no final.')
+        total = valor - desconto5
         break
     elif opção == 3:
-        
-        print(f'Sua compra de R${valor:.2f} vai custar R${valor - desconto10:.2f} no final.')
+        total = valor
+        parcela = valor / 2
+        print(f'Sua compra será parcelada em 2x de R${parcela:.2f} SEM JUROS!')
         break
     elif opção == 4:
         parcelas = int(input('Quantas parcelas vão ser? '))
-        print(f'Sua compra será parcelada em {parcelas}x de R${(valor + juros20) / parcelas:.2f} COM JUROS!')
-        print(f'Sua compra de R${valor:.2f} vai custar R${valor + juros20:.2f} no final.')
+        total = (valor + juros20) / parcelas
+        print(f'Sua compra será parcelada em {parcelas}x de R${total:.2f} COM JUROS!')
         break
     else:
         print('Escolha uma opção válida!')
         print('')
+print(f'Sua compra de R${valor:.2f} vai custar R${total:.2f} no final.')
